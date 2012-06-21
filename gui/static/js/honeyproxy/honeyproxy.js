a22aeb4d803b3c32c55186ed85855cb29c7f4c5b
Maximilian Hils <git@maximilianhils.com>
2012-06-21 03:31:15 +0200

    Added Log|Preview splitter. While the current solution works, the jQuery plugin is quite uncomfortable. Let's check whether we can find a better one.

--- gui/static/js/honeyproxy/honeyproxy.js
+++ gui/static/js/honeyproxy/honeyproxy.js
@@ -4,23 +4,23 @@ var HoneyProxy = {
 	
 _.extend(HoneyProxy, Backbone.Events);
 
+
+
+//debug
+window.HoneyProxy = HoneyProxy;
+
 $(function(){
 	
+	$("#splitter").height($(window).height());
+	
+	$("#splitter").splitter({outline: true,sizeLeft: $(window).width()-100, minLeft: 500});
+	
 	HoneyProxy.traffic = new HoneyProxy.Traffic;
-	
-	HoneyProxy.websocket.initialize();
-	
 	HoneyProxy.on("authenticated",function(){
 		console.time("fetch");
 		HoneyProxy.traffic.fetch();
 	});
-	
+	HoneyProxy.websocket.initialize();
 	HoneyProxy.trafficView = new HoneyProxy.TrafficView({collection: HoneyProxy.traffic, el: $("#traffic")[0]});	
-
 	
-	//debug
-	window.HoneyProxy = HoneyProxy;
-
-	//traffic.on("all",function(f){console.warn(arguments)});
-
 });
