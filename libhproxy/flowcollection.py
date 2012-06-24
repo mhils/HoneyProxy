2c7f996e07f9f5d0886ab258012f878a5132d2db
Maximilian Hils <git@maximilianhils.com>
2012-06-24 02:24:16 +0200

    Fix FlowCollection to store content unencoded.

--- libhproxy/flowcollection.py
+++ libhproxy/flowcollection.py
@@ -1,5 +1,5 @@
-#import json
-
+from libmproxy import encoding
+from libmproxy.flow import ODictCaseless
 class FlowCollection:
     def __init__(self):
         #self._flows = []
@@ -24,6 +24,14 @@ class FlowCollection:
     def addFlow(self, flow):
         flowRepr = flow._get_state()
         flowRepr["id"] = len(self._flows_json)
+        
+    
+        enc = flow.response.headers.get("content-encoding")
+        if enc and enc[0] != "identity":
+            decoded = encoding.decode(enc[0], flow.response.content)
+            if decoded:
+                flowRepr["response"]["content"] = decoded
+        
         #self._flows_json.append(json.dumps(flowRepr,ensure_ascii=None))
         self._flows_json.append(flowRepr)
         return len(self._flows_json)-1
