1e7902f0bcbd88ed1a4ae5f017debf646035d47f
Maximilian Hils <git@maximilianhils.com>
2012-07-12 23:54:04 +0200

    Improve prettyPrinting Removes ability to prettyPrint large chunks of data for performance reasons.

--- gui/js/honeyproxy/flows/PrettyFlow.js
+++ gui/js/honeyproxy/flows/PrettyFlow.js
@@ -1,12 +1,19 @@
 HoneyProxy.PrettyFlow = HoneyProxy.DocumentFlow.extend({
 	getPreview: function(callback){
 		return HoneyProxy.DocumentFlow.prototype.getPreview.call(this, function($pre){
-			$pre.addClass("prettyprint");
-			console.warn($pre.html().length);
-			if($pre.html().length < 1024*15)
+			$pre.addClass("prettyprint linenums");
+			
+			var autoPretty = 1024*15, 
+			askPretty = 1024*300, //don't even try to highlight very big files.
+			contentLength = $pre.html().length;
+			console.warn(contentLength);
+			if(contentLength < autoPretty) {
 				window.setTimeout(prettyPrint,1);
-			else {
-				$pre.before($("<div>").addClass("pre-menu-item").text("Prettify").click(prettyPrint));
+			} else if(contentLength < askPretty){
+				$pre.before($("<div>").addClass("pre-menu-item").text("Prettify (slow)").click(function(){
+					prettyPrint();
+					$(this).hide('slow');
+				}));
 			}
 		});
 	}
