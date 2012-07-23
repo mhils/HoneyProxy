ed6bdbe77630d3ee59eb52d5e16ab92f2a7da0e5
Maximilian Hils <git@maximilianhils.com>
2012-07-23 21:02:02 +0200

    Add well-tested dir dumper.
    It has been quite painful due to lots of caveats, but I assume that most
    bugs are gone now.

--- libhproxy/dirdumper.py
+++ libhproxy/dirdumper.py
@@ -5,6 +5,8 @@ class DirDumper:
         self.path = os.path.abspath(path)
 
     def add(self, flow):
+        if(len(flow.response.content) == 0):
+            return
         content = flow.response.content
         enc = flow.response.headers.get("content-encoding")
         if enc and enc[0] != "identity":
@@ -12,17 +14,72 @@ class DirDumper:
             if decoded:
                 content = decoded
         
-        #FIXME: proper filename 
+        #get host and path
+        host = flow.request.host
+        if(flow.request.port != 80):
+            host += "-"+str(flow.request.port)
+        path = flow.request.path.split("#")[0].split("?")[0].lstrip("/\\")
+        if(path == ""):
+            path = "__root__"
         
-        filename = os.path.join(self.path,flow.request.host,"response.txt")
+        #subdir is our relative path
+        subdir = os.path.join(host,path)
         
-        directory = os.path.split(filename)[0]
-        if not os.path.isdir(directory):
-            os.makedirs(directory)
+        #forbid relative directory changes.
+        subdir = "/".join(i.lstrip(".") for i in subdir.replace("\\","/").split("/"))
+        subdir = "/".join(i[:50]+"[HoneyProxy - cut off]" if (len(i) > 50) else i for i in subdir.split("/"))
+        
+        #remove invalid characters
+        subdir = os.path.normpath("".join(i for i in subdir if i not in r':*?"<>|'))
+        
+        #cut off too long filenames
+        MAX_DIR_LENGTH  = 200
+        MAX_FILE_LENGTH = 200
+        MAX_EXT_LENGTH  = 30
+        if(len(subdir) > MAX_DIR_LENGTH):
+            if(subdir[MAX_DIR_LENGTH] == "/"):
+                subdir = subdir[0:MAX_DIR_LENGTH+1]
+            else:
+                subdir = subdir[0:MAX_DIR_LENGTH]
+            subdir += "[HoneyProxy - cut off]"
+        
+        #ensure that subdir is relative, otherwise it could exploit outside of self.path
+        #os.path.join(foo,"/bar") => "/bar"
+        subdir = os.path.normpath("./"+subdir)
+        
+        filename = os.path.join(self.path,subdir)
+        
+        #We have the problematic situation that a both foo.com/bar
+        #and foo.com/bar/baz can be both valid files.
+        #However, we cannot create both a folder and a file both called "baz" in the same directory
+        #A possible approach would be using folders for everything and placing __resource__ files in them.
+        #While this would be a much consistent structure, it doesn't represent the file system very well.
+        #As this view is for visualization purposes only, we took the approach to append [dir] to conflicting folders.
+        #to accomplish this, we use a slightly modified version of os.makedirs
+        def makedirs(directory):
+            head,tail = os.path.split(directory)
+            if not os.path.isdir(head):
+                head = makedirs(head)
+                directory = os.path.join(head,tail)
+            if(os.path.isfile(directory)): #our special case - rename current dir
+                tail += "[dir]"
+                directory = os.path.join(head,tail)
+                return makedirs(directory)
+            if(not os.path.isdir(directory)):
+                os.mkdir(directory)  
+            return directory
+        d, filename = os.path.split(filename)
+        filename = os.path.join(makedirs(d),filename)
         
         filename, ext = os.path.splitext(filename)
-        
+        if(len(filename) > MAX_DIR_LENGTH+MAX_FILE_LENGTH):
+            filename = filename[0:MAX_DIR_LENGTH+MAX_FILE_LENGTH]+"[...]"
+        if(len(ext) >= MAX_EXT_LENGTH):
+            ext = "[..]" + ext[-MAX_EXT_LENGTH:]
         appendix = ""
+        
+        if(os.path.isdir(filename+ext)):
+            os.rename(filename+ext, filename+ext+"[dir]")
         while(os.path.isfile(filename+str(appendix)+ext)):
             with open(filename+str(appendix)+ext) as f:
                 s = f.read()
@@ -34,6 +91,6 @@ class DirDumper:
                 appendix += 1
             
         filename = filename + str(appendix) + ext
-            
-        with open(filename, 'w') as f:
-            f.write(content)
+                    
+        with open(filename, 'wb') as f:
+            f.write(str(content))
