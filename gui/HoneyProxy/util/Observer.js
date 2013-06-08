8e2c99fd1791dcbf1c2baa0c3462c16da5adcfd9
Maximilian Hils <git@maximilianhils.com>
2013-06-09 01:43:07 +0200

    rewrite Observer polyfill to be fully compatible with ES.next

--- gui/HoneyProxy/util/Observer.js
+++ gui/HoneyProxy/util/Observer.js
@@ -1,45 +1,55 @@
 /**
  * "Polyfill" for Harmony Object.observe
  * The module is designed to be replaced by Object.observe as soon as the harmony spec is finalized.
- * 
+ *
  */
-define(["dojo/_base/declare"],function(declare){
+define(["dojo/_base/declare"], function(declare) {
   "use strict";
-  
+
   var Observer = {};
-  
-  Observer.polyfillMixin = declare([],{
-    refresh: function(){
+
+  Observer.ObservablePolyfillMixin = declare([], {
+    constructor: function() {
+      Object.defineProperty(this, "_observe", {
+        value: [],
+        configurable: false,
+        enumerable: false,
+        writable: false
+      });
+    },
+    notify: function(record) {
       var self = this;
-      for(var watched_prop in this._watchCallbacks) {
-        var callbacks = this._watchCallbacks[watched_prop];
-        callbacks.forEach(function(callback){
-          var prop_name = watched_prop.replace(/^_/,"");
-          callback(prop_name , undefined, self[prop_name]);
-        });
-      }
+      this._observe.forEach(function(observer){
+        observer.call(self, record);
+      });
     }
   });
-  
-  var unobservable = {
-    remove: function(){}
+
+
+
+  Observer.observe = function(obj, observer) {
+    obj._observe.push(observer);
   };
-  
+  Observer.unobserve = function(obj, observer) {
+    var index = obj._observe.indexOf(observer);
+    obj._observe.splice(index,1);
+  };
+  /*
   Observer.observeProperty = function(obj, prop, callback) {
-    if(obj.watch) {
-      var handle = obj.watch(prop,function(name, oldValue, newValue){
-        callback.call(this, {
-          type: "updated",
-          object: obj,
-          name: name,
-          oldValue: oldValue
-        });
-      });
-      return { remove: function(){
-        handle.remove();
-      }};
-    }
-    return unobservable;
+    var observer = function(records) {
+      if (records.name === prop) {
+        callback.apply(this, arguments);
+      }
+    };
+    Observer.observe(obj,observer);
+    return {
+      remove: function() {
+        Observer.unobserve(obj,observer);
+      }
+    };
   };
+  */
+
+
   return Observer;
 });
